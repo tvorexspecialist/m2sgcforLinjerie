06b5415b8cfc70821e9a9762bac78fd4d7faaffe
tspecialist <tvorexspecialist@gmail.com>
2018-09-28 20:57:58 +0800

    Remove type stuff

--- extension/products/helpers/exportAsObject.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {object} context
- * @param {object} input
- * @param {object} input.element
- * @param {function} cb
- */
-module.exports = function (context, input, cb) {
-  cb(null, input.element)
-}
