0c21294c7b939d4cac77e04439d92f6d25b04d79
tspecialist <tvorexspecialist@gmail.com>
2018-10-13 17:07:32 +0800

    Add clearStorageKeys pipeline

---
+++ extension/helpers/clearStorageKeys.js
@@ -0,0 +1,11 @@
+/**
+ * Deletions contains an array of objects with the following structure
+ * {'storage': 'device', 'key': 'token'}
+ * @param {object} context
+ * @param {object} input
+ * @param {object[]} input.deletions
+ * @param {function} cb
+ */
+module.exports = function (context, input, cb) {
+  return cb(new Error('not implemented'))
+}
