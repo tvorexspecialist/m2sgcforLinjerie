5b82ff4d0ed08a1b6f0845d68f2b7d9321a5c1e7
tspecialist <tvorexspecialist@gmail.com>
2018-10-13 23:22:01 +0800

    Update certId handling

--- extension/cart/getCartIdFromStorage.js
+++ extension/cart/getCartIdFromStorage.js
@@ -1,12 +1,17 @@
+const CARTID_KEY = 'cartId'
+
 /**
  * @param {object} context
  * @param {object} input
  * @param {function} cb
  */
 module.exports = function (context, input, cb) {
-  const storage = context.storage.device
-  const key = 'cartId'
-  storage.get(key, (err, cartId) => {
+  const isLoggedIn = !!input.sgxsMeta.userId
+
+  let storageName = isLoggedIn ? 'user' : 'device'
+  const storage = context.storage[storageName]
+
+  storage.get(CARTID_KEY, (err, cartId) => {
     if (err) return cb(err)
     context.log.debug(`Got cartId ${cartId} from storage`)
     return cb(null, {cartId: cartId || null})
