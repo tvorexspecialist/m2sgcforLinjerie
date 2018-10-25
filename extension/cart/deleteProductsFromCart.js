c3e178b2ec4ba00e8fcca9b298f7f19bcb0c9369
tspecialist <tvorexspecialist@gmail.com>
2018-10-25 21:00:28 +0800

    Deleting items works without unit tests

---
+++ extension/cart/deleteProductsFromCart.js
@@ -0,0 +1,41 @@
+/**
+ * @param {object} context
+ * @param {object} input
+ * @param {function} cb
+ */
+module.exports = function (context, input, cb) {
+  const request = context.tracedRequest
+  const cartUrl = context.config.magentoUrl + '/carts'
+  const accessToken = input.token
+  const cartId = input.cartId
+  const cartItemIds = input.cartItemIds
+
+  if (!input.cartId) { cb(new Error('cart id missing')) }
+
+  deleteProductsFromCart(request, accessToken, cartId, cartItemIds, cartUrl, (err) => {
+    if (err) return cb(err)
+    cb(null, {})
+  })
+}
+
+function deleteProductsFromCart (request, accessToken, cartId, cartItemIds, cartUrl, cb) {
+  const options = {
+    url: `${cartUrl}/${cartId}/items`,
+    headers: {authorization: `Bearer ${accessToken}`},
+    qs: {
+      cartItemIds: cartItemIds.join(',')
+    },
+    json: true
+  }
+
+  console.log(require('util').inspect(options, false, 5))
+
+  request('magento:deleteItemsFromCart').delete(options, (err, res, body) => {
+    if (err) return cb(err)
+    if (res.statusCode !== 200) {
+      return cb(new Error(`Got ${res.statusCode} from magento: ${JSON.stringify(body)}`))
+    }
+
+    cb(null)
+  })
+}
